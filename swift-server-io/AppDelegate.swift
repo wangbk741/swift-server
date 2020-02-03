de8fb5d900ec0630b821da098498997361e1a2fa
wbk <291271698@qq.com>
2020-02-03 13:59:39 +0800

    Initial Commit

---
+++ swift-server-io/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  swift-server-io
+//
+//  Created by apple1 on 2020/2/3.
+//  Copyright © 2020 apple1. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ swift-server-io/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  swift-server-io
+//
+//  Created by apple1 on 2020/2/3.
+//  Copyright © 2020 apple1. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
